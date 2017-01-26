b62a49442532c3da048c4387ec9e3f77d2998a17
root <clay.lewis112@gmail.com>
2017-01-25 20:25:42 -0800

    added algorithim for random position

--- models/connectors.js
+++ models/connectors.js
@@ -1,11 +1,9 @@
 // object constructor and methods for child Connector object
-Connector.prototype = new Node();
-Connector.prototype.constructor = Connector;
-	function Connector(){
-	
-	}
-		Connector.prototype.draw = function (node1, node2){
-			line(node1.x, node1.y, node2.x, node2.y);
-		}
-
-
+// Connector.prototype = new Node();
+// Connector.prototype.constructor = Connector;
+// 	function Connector(){
+//
+// 	}
+// 		Connector.prototype.draw = function (node1, node2){
+// 			line(node1.x, node1.y, node2.x, node2.y);
+// 		}
--- models/nodes.js
+++ models/nodes.js
@@ -1,17 +1,43 @@
+balls = [];
+angles = [];
 // object constructor and methods for Node object
-function Node(tempX, tempY, tempDiameter,offSet, angle) {
+function Node(tempX, tempY) {
 	this.x = tempX;
 	this.y = tempY;
-	this.diameter = tempDiameter;
-	this.off = offSet;
-	this.a = angle
-	this.scalar = 70;
+	this.diameter = 30;
+	this.a = 0.0;
+	this.scalar = 5;
 	this.speed = 0.04;
 }
+// this method uses an algorithim to give random starting coordinates for the nodes
+	Node.prototype.calcpos = function(nodes) {
+		arr = [100, 200, 60, 100, 300, 69, 420];
+		var rand = arr[Math.floor(Math.random() * arr.length)];
+		for (var i = 0; i < nodes.length; i++){
+				var d = distSq(nodes[0], nodes[i]);
+				if (d >= rand) {
+					balls.push(nodes[i]);
+				}
+		}
+	}
 	Node.prototype.display = function() {
 		ellipse(this.x, this.y, this.diameter, this.diameter);
 	}
 	Node.prototype.wave = function() {
 		 this.a += this.speed;
-		this.y = this.off + sin(this.a) * this.scalar;
+		this.y += sin(this.a) * this.scalar;
+	}
+	//this method is for trying to increase the angular velocity of each node, so they all dont move at the same veloctiy
+	Node.prototype.calcangle = function() {
+		for(var i = 0; i< balls.length; i++){
+			var r = random(0.0, 2.0);
+			angles.push(r);
+		}
+		angles.sort(function(a, b){return a - b})
+	}
+	function distSq(a, b) {
+	  		var dx = b.x - a.x;
+	  		var dy = b.y - a.y;
+	  		var c = Math.sqrt( dx * dx + dy * dy);
+	  		return c;
 	}
